1ec17f56deb231b3c08343bb61fa91a7f0779120
developer-3108 <developer.akshat310804@gmail.com>
2025-11-05 21:03:32 +0530

    Initial Commit

---
+++ Sounair/SounairApp.swift
@@ -0,0 +1,17 @@
+//
+//  SounairApp.swift
+//  Sounair
+//
+//  Created by Akshat Srivastava on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SounairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
